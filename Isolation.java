6c40fbfb933aefee8849df616a067cbb369712a3
Redster11 <42357111+Redster11@users.noreply.github.com>
2020-04-11 22:13:03 -0700

    made board and movement system
    the board is now an 8x8 and we are able to move a piece and place an # as the move is made

--- Isolation.java
+++ Isolation.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 class Board
 {
@@ -19,13 +20,43 @@ class Board
         {
             if(team == 'O')
             {
-                this.board[this.teamO[0]][this.teamO[1]] = '#';
-                this.board[newLocation[0]][newLocation[1]] = 'O';
+                if(this.board[newLocation[0]][newLocation[1]] == 'O')
+                {
+                    System.out.println("Please move the piece");
+                    return false;
+                }
+                else if(this.board[newLocation[0]][newLocation[1]] != 'X')
+                {
+                    this.board[this.teamO[0]][this.teamO[1]] = '#';
+                    this.board[newLocation[0]][newLocation[1]] = 'O';
+                    this.teamO[0] = newLocation[0];
+                    this.teamO[1] = newLocation[1];
+                }
+                else
+                {
+                    System.out.println("Location already in use");
+                    return false;
+                }
             }
             else
             {
-                this.board[this.teamX[0]][this.teamX[1]] = '#';
-                this.board[newLocation[0]][newLocation[1]] = 'X';
+                if(this.board[newLocation[0]][newLocation[1]] == 'X')
+                {
+                    System.out.println("Please move the piece");
+                    return false;
+                }
+                else if(this.board[newLocation[0]][newLocation[1]] != 'O')
+                {
+                    this.board[this.teamX[0]][this.teamX[1]] = '#';
+                    this.board[newLocation[0]][newLocation[1]] = 'X';
+                    this.teamX[0] = newLocation[0];
+                    this.teamX[1] = newLocation[1];
+                }
+                 else
+                {
+                    System.out.println("Location already in use");
+                    return false;
+                }
             }
         }
         else {
@@ -36,8 +67,8 @@ class Board
     }
     public void initialPlace()
     {
-        this.teamO = new int[]{0, 0};
-        this.teamX = new int[]{this.n-1, this.n-1};
+        this.teamX = new int[]{0, 0};
+        this.teamO = new int[]{this.n-1, this.n-1};
         this.board[teamO[0]][teamO[1]] = 'O'; // team O
         this.board[teamX[0]][teamX[1]] = 'X'; // team X
     }
@@ -47,7 +78,7 @@ class Board
         {
             for(int i = 0; i < this.n; i++)
             {
-                System.out.print("  " + this.board[i][j]);
+                System.out.print("  " + this.board[j][i]);
             }
             System.out.println();
         }
@@ -57,20 +88,96 @@ class Board
 public class Isolation
 {
     Board board = new Board(8);
-    
+    /*
+    A == 0
+    B == 1
+    C == 2
+    D == 3
+    E == 4
+    F == 5
+    G == 6
+    H == 7
+    */
+    public static int[] getCoordinates(String Move)// from form D4 to numbers so should be 3,4
+    {
+        int height = Move.toUpperCase().charAt(0);
+        int length = Integer.parseInt(String.valueOf(Move.charAt(1)));
+        if(height < 65 || height > 72 || length > 7) // greater than H
+        {
+            //System.out.println("We failed because the values are: "+ height+", "+ length);
+            return new int[]{-1};
+        }
+        else
+        {
+            //System.out.println("Did not fail is valid number: " + height  + ", " + length);
+            return new int[] {height-65, length };
+        }
+    }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         Isolation iso = new Isolation();
         iso.board.printBoard();
-        int[] move = new int[] { 6, 6 };
-        if (iso.board.movePiece(move, 'X'))
+        boolean isTeamXTurn = true;
+        Scanner kb = new Scanner(System.in);
+        
+        for(int run = 0; run < 4; run++)
         {
-            System.out.println("\nThe TeamX piece has been moved to: " + move[0] + ", " + move[1]);
-            iso.board.printBoard();
+            boolean gettingMove = true;
+            if (isTeamXTurn)// x is comp eventually
+            {
+                int[] move = new int[2];
+                while(gettingMove)
+                {
+                    System.out.print("Player X Please enter your move: ");
+                    String XMove = kb.nextLine();
+                    move = getCoordinates(XMove);
+                    if(move[0] != -1)
+                    {
+                        if (iso.board.movePiece(move, 'X'))
+                        {
+                            System.out.println("\nThe TeamX piece has been moved to: " + move[0] + ", " + move[1]);
+                            iso.board.printBoard();
+                            gettingMove = false;
+                            isTeamXTurn = false;
+                        }
+                    }
+                    else
+                    {
+                        System.out.println("Please make sure that the values are on the board");    
+                    }
+                }
+                
+                //Change them to a x y coordinate system
+                
+                
+                isTeamXTurn = false;
+            }
+            else
+            {
+                int[] move = new int[2];
+                while(gettingMove)
+                {
+                    System.out.print("Player O Please enter your move: ");
+                    String OMove = kb.nextLine();
+                    move = getCoordinates(OMove);
+                    if(move[0] != -1){
+                        if (iso.board.movePiece(move, 'O'))
+                        {
+                            System.out.println("\nThe TeamO piece has been moved to: " + move[0] + ", " + move[1]);
+                            iso.board.printBoard();
+                            gettingMove = false;
+                            isTeamXTurn = true;
+                        }
+                    }
+                    else
+                    {   
+                        System.out.println("Please make sure that the values are on the board");
+                    }
+                    
+                }
+            }
+        
         }
-        else   
-            System.out.println("Please enter a different move");
     }
-    
-
 }
