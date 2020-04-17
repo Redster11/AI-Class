98401b27bc49f9c72cfe9dafbbf1f08e39221d07
Redster11 <42357111+Redster11@users.noreply.github.com>
2020-04-17 16:55:13 -0700

    Print Function and Move saving
    Added to the PrintBoard Function and also made a way to save the moves that were made by each player

--- Isolation.java
+++ Isolation.java
@@ -1,4 +1,6 @@
+import java.lang.reflect.Array;
 import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 class Board
@@ -7,6 +9,7 @@ class Board
     char [][] board; // blocked spots
     int[] teamX = new int[2];
     int[] teamO = new int[2];
+    
     public Board(int n)
     {
         this.n = n;
@@ -72,18 +75,42 @@ class Board
         this.board[teamO[0]][teamO[1]] = 'O'; // team O
         this.board[teamX[0]][teamX[1]] = 'X'; // team X
     }
-    public void printBoard()
+    public void printBoard(Moves Computer, Moves Opponent)
     {
+        System.out.print("   1  2  3  4  5  6  7  8");
+        if(Computer.getMoves().isEmpty())
+            System.out.println();
+        else
+            System.out.println("\tComputer  vs.  Opponent");
         for (int j = 0; j < this.n; j++)
         {
+            int letter = j + 65;
+            System.out.print((char)(letter));
             for(int i = 0; i < this.n; i++)
             {
                 System.out.print("  " + this.board[j][i]);
             }
+            if(!Computer.getMoves().isEmpty() && j < Computer.getMoves().size())
+                System.out.print("\t" + (j+1) + ". " + Computer.getMoves().get(j));
+            if(!Opponent.getMoves().isEmpty() && j < Opponent.getMoves().size())
+                System.out.print("\t\t  " + Opponent.getMoves().get(j));
             System.out.println();
         }
     }
     
+}
+class Moves
+{
+    ArrayList<String> moves = new ArrayList<>();
+    void add(String move)
+    {
+        moves.add(move);
+
+    }
+    ArrayList<String> getMoves()
+    {
+        return moves;
+    }
 }
 public class Isolation
 {
@@ -98,26 +125,34 @@ public class Isolation
     G == 6
     H == 7
     */
-    public static int[] getCoordinates(String Move)// from form D4 to numbers so should be 3,4
+    static Moves Computer = new Moves();
+    static Moves Oppenent = new Moves();
+
+    public static int[] getCoordinates(String Move, Boolean isXTurn)// from form D4 to numbers so should be 3,4
     {
         int height = Move.toUpperCase().charAt(0);
         int length = Integer.parseInt(String.valueOf(Move.charAt(1)));
-        if(height < 65 || height > 72 || length > 7) // greater than H
+        if (height < 65 || height > 72 || length > 8) // greater than H
         {
-            //System.out.println("We failed because the values are: "+ height+", "+ length);
-            return new int[]{-1};
-        }
-        else
-        {
-            //System.out.println("Did not fail is valid number: " + height  + ", " + length);
-            return new int[] {height-65, length };
+            // System.out.println("We failed because the values are: "+ height+", "+
+            // length);
+            return new int[] { -1 };
+        } else {
+            // System.out.println("Did not fail is valid number: " + height + ", " +
+            // length);
+            if (isXTurn)
+                Computer.add(Move);
+            else
+                Oppenent.add(Move);
+
+            return new int[] {height-65, length - 1 };
         }
     }
 
     public static void main(String[] args) 
     {
         Isolation iso = new Isolation();
-        iso.board.printBoard();
+        iso.board.printBoard(iso.Computer, iso.Oppenent);
         boolean isTeamXTurn = true;
         Scanner kb = new Scanner(System.in);
         
@@ -131,13 +166,13 @@ public class Isolation
                 {
                     System.out.print("Player X Please enter your move: ");
                     String XMove = kb.nextLine();
-                    move = getCoordinates(XMove);
+                    move = getCoordinates(XMove, isTeamXTurn);
                     if(move[0] != -1)
                     {
                         if (iso.board.movePiece(move, 'X'))
                         {
-                            System.out.println("\nThe TeamX piece has been moved to: " + move[0] + ", " + move[1]);
-                            iso.board.printBoard();
+                            System.out.println("\nThe TeamX piece has been moved to: " + move[0] + ", " + (move[1] + 1));
+                            iso.board.printBoard(iso.Computer, iso.Oppenent);
                             gettingMove = false;
                             isTeamXTurn = false;
                         }
@@ -160,12 +195,12 @@ public class Isolation
                 {
                     System.out.print("Player O Please enter your move: ");
                     String OMove = kb.nextLine();
-                    move = getCoordinates(OMove);
+                    move = getCoordinates(OMove, isTeamXTurn);
                     if(move[0] != -1){
                         if (iso.board.movePiece(move, 'O'))
                         {
-                            System.out.println("\nThe TeamO piece has been moved to: " + move[0] + ", " + move[1]);
-                            iso.board.printBoard();
+                            System.out.println("\nThe TeamO piece has been moved to: " + move[0] + ", " + (move[1] + 1));
+                            iso.board.printBoard(iso.Computer, iso.Oppenent);
                             gettingMove = false;
                             isTeamXTurn = true;
                         }
